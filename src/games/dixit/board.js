296e0808091609a32b91b8ae866fc51c0c85daaa
nagyadam2092 <nagyadam2092@gmail.com>
2020-04-12 08:37:23 +0200

    working base game, still need to update cards and scoring - end clean up comments

--- src/games/dixit/board.js
+++ src/games/dixit/board.js
@@ -25,9 +25,10 @@ export class DixitBoard extends React.Component {
     componentDidMount() {
         console.log(this.props.isActive);
         if (this.props.isActive) {
-            this.props.events.setStage('masterChooser');
-        } else {
-            this.props.events.setStage('noop');
+            // this.props.events.setStage('masterChooser');
+            // this.props.events.setActivePlayers({
+            //     currentPlayer: {stage: 'masterChooser'},
+            // });
         }
     }
 
@@ -36,17 +37,84 @@ export class DixitBoard extends React.Component {
             console.log('its not your turn');
             return;
         }
-        console.log(id);
-        this.props.moves.ChooseCard(id);
-        this.props.events.setActivePlayers({
-            currentPlayer: 'noop',
-            others: 'trickChooser',
-        });
+        if (this.props.isActive) {
+            if (this.isMasterPlayer(this.props.ctx, this.props.playerID)) {
+                this.props.moves.SetMasterCard(id);
+                // this.props.events.setActivePlayers({
+                //     others: 'trickChooser',
+                //     moveLimit: 1,
+                // });
+            } else if (this.isTrickingPlayer(this.props.ctx, this.props.playerID)) {
+                console.log('trying to trick');
+                this.props.moves.TrickCard(id, this.props.playerID);
+                console.log('this.props.G.tricks', this.props.G.tricks);
+                console.log('Object.values(this.props.G.tricks).filter(x => x).length === this.props.ctx.numPlayers - 2', Object.values(this.props.G.tricks).filter(x => x).length === this.props.ctx.numPlayers - 2);
+                // if (Object.values(this.props.G.tricks).filter(x => x).length === this.props.ctx.numPlayers - 2) {
+                //     console.log('SETTING ACTIVE PLAYERS');
+                //     this.props.events.endStage();
+                //     // this.props.events.setActivePlayers({
+                //     //     others: 'vote',
+                //     //     moveLimit: 1,
+                //     // });
+                // }
+                // this.props.events.setActivePlayers({
+                //     others: 'vote',
+                // });
+                // if (this.shouldTrickStageEnd(this.props.G.tricks)) {
+                //     this.props.events.endStage();
+                // }
+            } else if (this.isVotingPlayer(this.props.ctx, this.props.playerID)) {
+                this.props.moves.VoteOnCard(id, this.props.playerID);
+                // if (this.props.ctx.activePlayers === null) {
+                //     this.props.events.setActivePlayers({
+                //         others: 'vote',
+                //         moveLimit: 1,
+                //     });
+                // }
+                // if (this.shouldVoteStageEnd(this.props.G.votes)) {
+                //     console.log('please end stage');
+                //     this.props.events.endStage();
+                //     console.log('please end turn');
+                //     this.props.events.endTurn();
+                // }
+            }
+        }
         // this.props.events.endTurn();
         // if (this.isActive(id)) {
         //     this.props.moves.clickCell(id);
         // }
     };
+
+    isMasterPlayer(ctx, id) {
+        return ctx.activePlayers && ctx.activePlayers[id] === 'masterChooser';
+    }
+
+    isTrickingPlayer(ctx, id) {
+        return ctx.activePlayers && ctx.activePlayers[id] === 'trickChooser';
+    }
+
+    isVotingPlayer(ctx, id) {
+        return ctx.activePlayers && ctx.activePlayers[id] === 'vote';
+    }
+
+    // shouldTrickStageEnd(tricks) {
+    //     return Object.values(tricks)
+    //         .filter((trick, idx) => idx > 0)
+    //         .every(trick => trick !== null);
+    // }
+    //
+    // shouldVoteStageEnd(votes) {
+    //     return Object.values(votes)
+    //         .filter((vote, idx) => idx > 0)
+    //         .every(vote => vote !== null);
+    // }
+
+    isVoteStage() {
+        // return false;
+        console.log(this.props.ctx);
+        return this.props.ctx.activePlayers && Object.values(this.props.ctx.activePlayers)
+            .every(stage => stage === 'vote');
+    }
 
     isActive(id) {
         if (!this.props.isActive) return false;
@@ -59,13 +127,25 @@ export class DixitBoard extends React.Component {
 
     render() {
         console.log(this);
+        console.log('this.props.isActive', this.props.isActive);
+        console.log('this.props.ctx.activePlayers', this.props.ctx.activePlayers);
+        const name = this.props.gameMetadata.find(player => +player.id === +this.props.playerID).name;
         const playerID = +this.props.playerID;
         const cardIds = this.props.G.cards.filter((card, idx) => (idx >= playerID * CARDS_IN_HAND) && (idx < (playerID + 1) * CARDS_IN_HAND))
 
         return (
             <div>
+                <pre>{name}</pre>
                 <pre>Turn nr: {this.props.ctx.turn}</pre>
                 {this.props.isActive && <h1>Choose a card!</h1>}
+                {this.isVoteStage() && <div>
+                    <h1>LET'S VOTE</h1>
+                    {this.props.G.cardsToVoteFor.map(id => <img key={id} className="card" src={this.getCardURL(id)} onClick={this.onClick.bind(this, id)}/>)}
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                </div>}
                 {cardIds.map(id => <img key={id} className="card" src={this.getCardURL(id)} onClick={this.onClick.bind(this, id)}/>)}
             </div>
         );
--- src/games/dixit/game.js
+++ src/games/dixit/game.js
@@ -1,98 +1,143 @@
-// function MasterChooser(G, ctx, id) {
-//     console.log('ChooseCard', G, ctx, id);
-// }
-// function TrickChooser(G, ctx, id) {
-//     console.log('ChooseCard', G, ctx, id);
-// }
-
-function ChooseCard(G, ctx, id) {
-    console.log('ChooseCard', G, ctx, id);
+function SetMasterCard(G, ctx, id) {
     return {
         ...G,
-        master: ctx.playOrderPos === 0 ? id : G.master,
-        tricks: ctx.playOrderPos > 0 ? {} : G.tricks,
+        master: id,
+        cardsToVoteFor: [id],
     };
 }
 
-function VoteOnCard(G, ctx) {
-    console.log('VoteOnCard', G, ctx);
+function TrickCard(G, ctx, cardId, playerId) {
+    console.log('GGGGGGG', G);
+    console.log('TrickCard G.tricks : ' , G.tricks);
+    return {
+        ...G,
+        tricks: {
+            ...G.tricks,
+            [playerId]: cardId,
+        },
+        // ctx.random.Shuffle
+        cardsToVoteFor: ctx.random.Shuffle([
+            ...G.cardsToVoteFor,
+            cardId,
+        ]),
+    };
 }
 
-function Noop(G, ctx) {
-    console.log('Noop', G, ctx);
+function VoteOnCard(G, ctx, cardId, playerId) {
+    return {
+        ...G,
+        votes: {
+            ...G.votes,
+            [playerId]: cardId,
+        }
+    };
 }
 
 export const Dixit = {
     name: "dixit",
 
     setup: (ctx) => {
-        console.log(ctx);
+        const tricksVotesEmptyObj = ctx.playOrder.reduce((acc, player) => {
+            acc[player] = null;
+            return acc;
+        }, {});
+        const scoresEmptyObj = ctx.playOrder.reduce((acc, player) => {
+            acc[player] = 0;
+            return acc;
+        }, {});
         return {
             cards: ctx.random.Shuffle(Array.from(Array(100).keys())),
             master: null,
-            tricks: ctx.playOrder.reduce((acc, player) => {
-                console.log(player, acc);
-                acc[player] = null;
-                return acc;
-            }, {}),
+            tricks: tricksVotesEmptyObj,
+            votes: tricksVotesEmptyObj,
+            scores: scoresEmptyObj,
+            cardsToVoteFor: [],
+            turn: 'masterChooser',
         }
     },
 
-    // phases: {
-    //     choose: {
-    //         moves: {
-    //             MasterChooser,
-    //         }
-    //     },
-    //     vote: {
-    //         moves: {
-    //             VoteOnCard,
-    //         }
-    //     }
-    //
-    // },
-
     turn: {
+        onBegin: (G, ctx) => {
+            ctx.events.setActivePlayers({
+                currentPlayer: {stage: 'masterChooser'},
+                moveLimit: 1,
+            });
+            return G;
+        },
         stages: {
             masterChooser: {
                 moves: {
-                    ChooseCard,
+                    SetMasterCard,
                 },
+                // moveLimit: 1,
+                next: 'trickChooser'
             },
             trickChooser: {
                 moves: {
-                    ChooseCard
-                }
-            },
-            noop: {
-                moves: {
-                    Noop,
-                }
+                    TrickCard,
+                },
+                moveLimit: 1,
+                next: 'vote'
             },
             vote: {
                 moves: {
                     VoteOnCard,
-                }
+                },
+                moveLimit: 1,
             },
+        },
+        onMove: (G, ctx) => {
+            if (ctx.activePlayers === null) {
+                const newTurn = G.turn === 'masterChooser' ? 'trickChooser' : G.turn === 'trickChooser' ? 'vote' : null;
+                ctx.events.setActivePlayers({
+                    others: newTurn,
+                    moveLimit: 1,
+                });
+                return {
+                    ...G,
+                    turn: newTurn,
+                }
+            }
+            return G;
+        },
+        onEnd: (G, ctx) => {
+            console.log('ON END TURN: ', G);
+            const tricksVotesEmptyObj = ctx.playOrder.reduce((acc, player) => {
+                acc[player] = null;
+                return acc;
+            }, {});
+
+
+            // deal 1 (different) new card to everyone
+            // TODO update scores
+            return {
+                ...G,
+                master: null,
+                tricks: tricksVotesEmptyObj,
+                votes: tricksVotesEmptyObj,
+                cardsToVoteFor: [],
+                turn: 'masterChooser'
+            };
+        },
+        endIf: (G, ctx) => {
+            console.log('Object.keys(G.votes)', Object.keys(G.votes));
+            console.log('ctx.currentPlayer', ctx.currentPlayer);
+            console.log('G.votes', G.votes);
+            return Object.keys(G.votes)
+                .filter((key) => +key !== +ctx.currentPlayer)
+                .every(key => G.votes[key] !== null);
         }
     },
 
-    // moves: {
-    //     // clickCell(G, ctx, id) {
-    //     //     if (G.cells[id] === null) {
-    //     //         G.cells[id] = ctx.currentPlayer;
-    //     //     }
-    //     // }
-    // },
-
-    // turn: { moveLimit: 1 },
-
     onEnd: (G, ctx) => {
+        console.log('END GAME G: ', G);
+        console.log('END GAME ctx: ', ctx);
+        return G;
         // TODO calculate scores;
     },
 
     endIf: (G, ctx) => {
-        console.log(ctx);
+        console.log('endIf?');
         return ctx.turn === 10;
     }
 };
