4c26acfcb0e1e257aa1fb179148e9abee48ad5a4
nagyadam2092 <nagyadam2092@gmail.com>
2020-04-12 18:51:51 +0200

    Revert "for heroku reasons copy the whole project under react-ui and server folders (I wouldn't like to stay that way..)"
    This reverts commit 502083b0

--- react-ui/src/games/dixit/cards/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const appDir = path.dirname(require.main.filename);
-const cardsDir = `${appDir}/../public/assets/img/cards`;
-
-export const cards = fs.readdirSync(cardsDir);
-
